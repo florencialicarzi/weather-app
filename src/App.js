64f61693e40767db6a532246569f801c9b7f0f92
florencialicarzi <florencia.licarzi13@gmail.com>
2022-03-09 16:01:23 -0300

    maquetado basico

--- src/App.js
+++ src/App.js
@@ -2,8 +2,26 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      Probando Git
+    <div className='App'>
+      <div className="left_container">
+        <div className='left_topbar'>
+          A
+        </div>
+        <div className='left_main'>
+          B
+        </div>
+      </div>
+      <div className='right_container'>
+        <div className='right_topbar'>
+          a
+        </div>
+        <div className='right_week'>
+          b          
+        </div>
+        <div className='right_hl'>
+          c          
+        </div>
+      </div>
     </div>
   );
 }
